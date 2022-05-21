40eaa58d8d74c835e026276df9e665562af664fb
organik-inc <luis@organik.express>
2022-05-21 15:57:06 +0000

    For now let's stay at 0.8.12 #noRagrets

--- contracts/reverseBids.sol
+++ contracts/reverseBids.sol
@@ -1,3 +1,3 @@
 // Visit beta.OK.gold from your phone to interact with this smart contract. Built by Organik, Inc. 2022
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12; // Thanks to Rahat for the tip @twitter/rahatcodes/status/1527266594774208515
+pragma solidity 0.8.12; // Thanks to Rahat for the tip @twitter/rahatcodes/status/1527266594774208515
